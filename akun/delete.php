0ff28b79ec8352b8af222562e82327ff8e9a4c16
rajwa123321 <rajwaaanggara123@gmail.com>
2025-07-12 01:04:33 +0700

    reset id di delete

--- akun/delete.php
+++ akun/delete.php
@@ -18,8 +18,14 @@ $id = $data['id'];
 
 $sql = "DELETE FROM login WHERE id=$id";
 if (mysqli_query($conn, $sql)) {
+  // Reset ID urut ulang
+  mysqli_query($conn, "SET @num := 0");
+  mysqli_query($conn, "UPDATE login SET id = @num := @num + 1");
+  mysqli_query($conn, "ALTER TABLE login AUTO_INCREMENT = 1");
+
   echo json_encode(["message" => "Akun berhasil dihapus"]);
 } else {
   echo json_encode(["message" => "Gagal menghapus akun"]);
 }
+
 ?>
